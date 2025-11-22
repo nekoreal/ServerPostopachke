256f5b1178509567d3f4ae5e7e3a5a63d907e699
root <root@dysfunctionalreading.aeza.network>
2025-11-22 11:23:04 +0100

    Рабочая версия на linux сервере

--- main.py
+++ main.py
@@ -29,5 +29,5 @@ app.register_blueprint(post_routes.post_bp)
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5001)
 
